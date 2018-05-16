4c344a153074ee68b380db837e797be5cf80ba58
imogine <hellogreenwhales@gmail.com>
2018-05-15 20:24:04 -0500

    Upload

---
+++ test.js
@@ -0,0 +1,41 @@
+//Setup
+var contacts = [{
+    "firstName": "Akira",
+    "lastName": "Laine",
+    "number": "0543236543",
+    "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+    "firstName": "Harry",
+    "lastName": "Potter",
+    "number": "0994372684",
+    "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+    "firstName": "Sherlock",
+    "lastName": "Holmes",
+    "number": "0487345643",
+    "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+    "firstName": "Kristian",
+    "lastName": "Vos",
+    "number": "unknown",
+    "likes": ["Javascript", "Gaming", "Foxes"]
+  }
+];
+
+var query=[];
+function lookUpProfile(firstName, prop) {
+  // Only change code below this line
+  for (var i = 0; i < contacts.length; i++) {
+    if (contacts[i.0] == firstName) {
+      query.push(contacts[i.0])
+    }
+  }
+  // Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");
+console.log(query)
